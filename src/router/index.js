f0b32e0e427c17171509687dc3b62a1a8b532a05
Renato Pereira <renato.19gp@gmail.com>
2023-03-19 16:21:00 -0300

    Mudando o home do projeto

--- src/router/index.js
+++ src/router/index.js
@@ -6,16 +6,16 @@ const routes = [
     path: '/',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
+      // {
+      //   path: '',
+      //   name: 'Filmes',
+      //   // route level code-splitting
+      //   // this generates a separate chunk (about.[hash].js) for this route
+      //   // which is lazy-loaded when the route is visited.
+      //   component: () => import(/* webpackChunkName: "home" */ '@/views/Filmes.vue'),
+      // },
       {
         path: '',
-        name: 'Filmes',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Filmes.vue'),
-      },
-      {
-        path: '/calculadora',
         name: 'Calculadora',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
